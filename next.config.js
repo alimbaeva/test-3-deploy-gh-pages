6f605531af3f4251eaa512ece071c1ab213baea7
Asel <asel.alimbaeva.kk@gmail.com>
2024-09-09 06:48:22 +0300

    test-1

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/test-3-deploy-gh-pages",
-  output: "export",
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
---
+++ out/_next/static/gIhB56ifTNu9gLO4gOna4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/69-51c1d4aa40f323ca.js","static/css/b83c48e97dfc9584.css","static/chunks/pages/index-a1b43057b6254f64.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
