fc31ed1924b3df66528b9bbd180d89b1a3e62ef3
Asel <asel.alimbaeva.kk@gmail.com>
2024-09-09 06:26:19 +0300

    test

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/test-3-deploy-gh-pages",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
